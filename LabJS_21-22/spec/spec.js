6cd8eb32cc83097663c438758fd7060d139eb4a3
zaytsevd <zaytsev.d@gmail.com>
2016-05-31 23:11:46 +0300

    final 21-22

--- LabJS_21-22/spec/spec.js
+++ LabJS_21-22/spec/spec.js
@@ -1,12 +1,42 @@
 var app = require('../js/script2.js');
 
 describe("app", function () {
-    it("It should return 16", function () {
+    it("Testing app x^y", function () {
         // prepare
         var result;
         //act
         result = app.pwr(4,2);
         //assert
         expect(result).toBe(16);
+        //act
+        result = app.pwr(3,3);
+        //assert
+        expect(result).toBe(27);
+        //act
+        result = app.pwr(-5,3);
+        //assert
+        expect(result).toBe(-125);
+        //act
+        //result = app.pwr(3,3);
+        //assert
+        //expect(result).toBe(33);
     });
+     it("It generates error", function () {
+         // prepare
+        var result;
+        //act
+        result = app.pwr(3,3);
+        //assert
+        expect(result).toBe(33);
+         
+     });
+     xit("It`s disabled", function () {
+         // prepare
+        var result;
+        //act
+        result = app.pwr(3,3);
+        //assert
+        expect(result).toBe(33);
+         
+     });
 });
